56fd1a88cc8d2c7ee5f2de8e13418fbed0c0ef3c
sonicashdev <sonicash.io.dev@gmail.com>
2025-08-03 13:03:05 +0300

    fix: Update GraphQL endpoint for allPools to correct URL

--- src/config/graphql.ts
+++ src/config/graphql.ts
@@ -5,7 +5,7 @@ export const GRAPHQL_ENDPOINTS = {
   1301: 'https://indexer.dev.hyperindex.xyz/771be56/v1/graphql', // Unichain Sepolia
   56: 'http://localhost:8080/api/rest/PoolManager_Initialize', // BNB Smart Chain
   all: 'http://localhost:8080/api/rest/Pool', // All pools, all networks
-  allPools:'https://indexer.dev.hyperindex.xyz/7c4efed/v1/graphql'
+  allPools:'https://indexer.dev.hyperindex.xyz/02a5e20/v1/graphql'
 } as const;
 
 // Network names for display
